dc52a73c3acc58770055304a82de933860f72a1c
Emmanuel Kerich <professionalcvwritersconsultan@gmail.com>
2025-09-09 21:13:04 +0300

    Test tickets

--- src/config/eventConfig.js
+++ src/config/eventConfig.js
@@ -2,7 +2,7 @@
 module.exports = {
   currency: "USD",
   ticketTypes: [
-    { code: "BUSINESS", name: "Business Ticket", price: 1000 },
+    { code: "BUSINESS", name: "Business Ticket", price: 100 },
     { code: "VIP", name: "VIP Ticket", price: 3000 },
   ],
 };
